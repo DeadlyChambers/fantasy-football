a0e1044b70c8f81f7462b98303b506a1b25ee07a
Shane Chambers <deadlychambers@outlook.com>
2020-12-20 05:28:21 -0700

    Initial commit

---
+++ SCC.FantasyFootball/SCC.FantasyFootball/Scripts/setupEF.ps1
@@ -0,0 +1,4 @@
+﻿dotnet restore
+#cd possibly
+#dir possibly
+dotnet ef dbcontext scaffold "Host=localhost;Database=postgres;Username=postgres;Password=Snowboard1" Npgsql.EntityFrameworkCore.PostgreSQL -o SCC.FantasyFootball.DataAccess
